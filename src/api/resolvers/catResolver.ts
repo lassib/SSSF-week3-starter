a6b516d684aa510d2989f62f677fa8132c027f4a
lassib <91030337+lassib@users.noreply.github.com>
2023-09-20 16:54:15 +0300

    base for resolvers

--- src/api/resolvers/catResolver.ts
+++ src/api/resolvers/catResolver.ts
@@ -8,3 +8,36 @@
 // 2.1. createCat
 // 2.2. updateCat
 // 2.3. deleteCat
+
+import catModel from '../models/catModel';
+import {Cat} from '../../interfaces/Cat';
+
+const catResolver = {
+  Query: {
+    async cats() {
+      return await catModel.find();
+    },
+    async catById(_: any, cat: Cat) {
+      return await catModel.findById(cat.id);
+    },
+    async catsByOwner(_: any, cat: Cat) {
+      return await catModel.find({owner: cat.owner});
+    },
+    async catsByArea(_: any, cat: Cat) {
+      return await catModel.find({area: cat.location});
+    },
+  },
+  Mutation: {
+    createCat: async (_: any, cat: Cat) => {
+      return await catModel.create(cat);
+    },
+    updateCat: async (_: any, cat: Cat) => {
+      return await catModel.findOneAndUpdate(cat.id, cat, {new: true});
+    },
+    deleteCat: async (_: any, cat: Cat) => {
+      return await catModel.findByIdAndRemove(cat.id);
+    },
+  },
+};
+
+export default catResolver;
--- src/api/resolvers/userResolver.ts
+++ src/api/resolvers/userResolver.ts
@@ -5,4 +5,31 @@
 // 2. Mutations
 // 2.1. createUser
 // 2.2. updateUser
+
+import userModel from '../models/userModel';
+import {User} from '../../interfaces/User';
+
 // 2.3. deleteUser
+const userResolver = {
+  Query: {
+    async users() {
+      return await userModel.find();
+    },
+    async userById(_: any, user: User['id']) {
+      return await userModel.findById(user.id);
+    },
+  },
+  Mutation: {
+    createUser: async (_: any, user: User) => {
+      return await userModel.create(user);
+    },
+    updateUser: async (_: any, user: User) => {
+      return await userModel.findByIdAndUpdate(user.id, user, {new: true});
+    },
+    deleteUser: async (_: any, user: User['id']) => {
+      return await userModel.findByIdAndRemove(user.id);
+    },
+  },
+};
+
+export default userResolver;
